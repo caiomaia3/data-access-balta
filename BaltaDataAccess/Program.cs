314f189db4258231916ba9b0da04c3a7a57fd25c
caiomaia3 <caiomaia3@gmail.com>
2022-01-07 15:25:15 -0300

    sql insert

--- BaltaDataAccess/Program.cs
+++ BaltaDataAccess/Program.cs
@@ -10,12 +10,46 @@ namespace BaltaDataAccess
         static void Main(string[] args)
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True";
+
+            var category = new Category();
+
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            const string insertSql = @"
+            INSERT INTO [Category]
+            VALUES (
+                @Id,
+                @Title,
+                @Url,
+                @Summary,
+                @Order,
+                @Description,
+                @Featured
+            )";
+
             using (var connection = new SqlConnection(connectionString))
             {
-                var categories = connection.Query<Category>("SELECT [Id],[Title] FROM [Category]");
-                foreach (var category in categories)
+                var rows = connection.Execute(insertSql, new
                 {
-                    System.Console.WriteLine($"{category.Id} - {category.Title}");
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                });
+                System.Console.WriteLine($"{rows} linhas inseridas.");
+                var categories = connection.Query<Category>("SELECT [Id],[Title] FROM [Category]");
+                foreach (var c in categories)
+                {
+                    System.Console.WriteLine($"{c.Id} - {c.Title}");
                 }
             }
         }
