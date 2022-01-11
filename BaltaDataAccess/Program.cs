4acc472cafdbf5cbb7a0377f5f477339ee138db8
caiomaia3 <caiomaia3@gmail.com>
2022-01-11 17:13:16 -0300

    like on dapper query

--- BaltaDataAccess/Program.cs
+++ BaltaDataAccess/Program.cs
@@ -24,7 +24,8 @@ namespace BaltaDataAccess
                 // OneToOne(connection);
                 // OneToMany(connection);
                 // QueryMultiple(connection);
-                SelectIn(connection);
+                // SelectIn(connection);
+                Like(connection, "api");
             }
         }
 
@@ -275,6 +276,19 @@ namespace BaltaDataAccess
                 "4327ac7e-963b-4893-9f31-9a3b28a4e72b",
                 "e6730d1c-6870-4df3-ae68-438624e04c72"
                 }
+            });
+            foreach (var item in items)
+            {
+                System.Console.WriteLine(item.Title);
+            }
+        }
+
+        static void Like(SqlConnection connection, string term)
+        {
+            var query = @"SELECT * FROM [Course] WHERE [Title] LIKE @exp";
+            var items = connection.Query<Career>(query, new
+            {
+                exp = $"%{term}%"
             });
             foreach (var item in items)
             {
