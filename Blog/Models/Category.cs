bb497920228ae6cef19f413841fb3ecd5ae204a9
caiomaia3 <caiomaia3@gmail.com>
2022-01-13 15:23:36 -0300

    User Category relation

--- Blog/Models/Category.cs
+++ Blog/Models/Category.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Blog.Interfaces;
 using Dapper.Contrib.Extensions;
 
@@ -9,5 +10,9 @@ namespace Blog.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
+        public List<Post> Posts { get; set; }
+
+        public Category() => Posts = new List<Post>();
+
     }
 }
